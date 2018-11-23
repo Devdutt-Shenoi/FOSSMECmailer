30dd923b2e56eeeb96bcf4d66ff50fcb44c387a1
annu12340 <43414928+annu12340@users.noreply.github.com>
2018-11-23 18:26:09 +0530

    made a few simple changes

--- mailman.py
+++ mailman.py
@@ -28,18 +28,20 @@ passwd = getpass.getpass("Password: ")
 with open("format.content") as file: 
    format = file.readlines() # Thus format now contains the content as well as the subject of the mail(row 0)
 
-# data.csv contains the details of all those recievng the email. The formais given below
+# data.csv contains the details of all those receiving the email. The formais given below
 """
 Name, Email, Extra (Details of no particular reason here)
 """
 with open('data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    # Below, each row is the detail of a user
+    # Below, each row contains the details of a user
     for row in csv_reader:
         mentor = mentor_contact[row[2]]
         print(f'Email sent to {row[0]} @ email {row[1]})
         sub = format[0]
         body = "".join(format[1:]).replace("{Name}", row[0]) # Take in body of email from content file and make manipulate it according to reciever.
         email = row[1] # The email of the reciever
-        send_email(email, passwd, email, sub, body)
+        send_email(email, passwd, recipient, sub, body)
+        server.quit()
+             
 
